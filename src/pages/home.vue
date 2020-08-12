e120bacfc71e309c32cbd8b74b468797a575c621
xlk <1148038404@qq.com>
2020-08-12 11:48:06 +0800

    搭建项目基本结构

---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view/>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+export default {
+  name:'home'
+}
+</script>
